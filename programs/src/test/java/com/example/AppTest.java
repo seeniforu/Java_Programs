96b18bee270212b0c89e673e486b8865f99856ed
Srinivasan <srinivasanforu7@gmail.com>
2024-11-18 08:17:43 +0530

    Remove special characters with regex

--- programs/src/test/java/com/example/AppTest.java
+++ programs/src/test/java/com/example/AppTest.java
@@ -44,4 +44,23 @@ public class AppTest
         System.out.println("Original Sentence: "+sentence);
         System.out.println("Duplicate removed: "+dupRemoved);
     }
+
+    @Test
+    public void removeSpecialCharater(){
+        /*
+         * Regex for space '\\s'
+         * With ^ - Remove everything other than this "[^a-zA-Z\\s]"
+         * Without ^ - Remove whatever mentioned in regex "[a-zA-Z\\s]"
+         */
+        String s = "!@#$%^&*(Hello there) [I'm Srini@#$%^vasa@#$%^n]";
+        
+        String regexOne = "[^a-zA-Z\\s]"; // Removes everything other than mentioned
+        String regexTwo = "[a-zA-Z\\s]"; // Removes everything mentioned
+        
+        String s1 = s.replaceAll(regexOne, "");
+        String s2 = s.replaceAll(regexTwo,"");
+
+        System.out.println(s1); // Hello thereIm Srinivasan
+        System.out.println(s2); //!@#$%^&*()['@#$%^@#$%^]
+    }
 }
